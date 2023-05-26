8d9ed9d43ab8a4019f1788214c90ea894e7395d2
Abdul Muhaimin <abdulmuhaimin97@gmail.com>
2023-05-26 14:55:19 +0800

    Newer Location Commit

--- samples/src/samples/Location.java
+++ samples/src/samples/Location.java
@@ -4,18 +4,13 @@ public class Location {
 	 private String name, email; // private = restricted access
 
 	  // Getter
-	  public String getName() {
-	    return name;
-	  }
-	  
+	 
 	  public String getEmail() {
 		    return email;
 		  }
 
 	  // Setter
-	  public void setName(String newName) {
-	    this.name = newName;
-	  }
+	  
 	  
 	  public void setEmail(String newEmail) {
 		    this.name = newEmail;
